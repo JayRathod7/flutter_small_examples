203eef43e1666134355506f931f5d149d508a4cb
Jay Rathod <107827602+JayRathod7@users.noreply.github.com>
2024-09-12 17:11:48 +0530

    add automated validation

--- validation/lib/register.dart
+++ validation/lib/register.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
@@ -14,100 +16,196 @@ class _RegisterState extends State<Register> {
   TextEditingController passwordController = TextEditingController();
   TextEditingController confirmPassController = TextEditingController();
 
+  var isUpperCase = false;
+  var isLowerCase = false;
+  var specialCharacter = false;
+  var oneLatter = false;
+  var character = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        body: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    "Register",
-                    style: TextStyle(
-                        fontSize: 50,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black45),
-                  ),
-                  const SizedBox(),
-                  TextField(
-                      controller: userNameController,
-                      decoration: const InputDecoration(
-                          hintText: "User name", border: OutlineInputBorder())),
-                  const SizedBox(height: 10),
-                  TextField(
-                      controller: mobileNumberController,
-                      keyboardType: TextInputType.number,
-                      decoration: const InputDecoration(
-                          hintText: "Mobile Numebr",
-                          border: OutlineInputBorder())),
-                  const SizedBox(height: 10),
-                  TextField(
-                      controller: emailController,
-                      decoration: const InputDecoration(
-                          hintText: "Email Id", border: OutlineInputBorder())),
-                  const SizedBox(height: 10),
-                  TextField(
-                      controller: passwordController,
-                      decoration: const InputDecoration(
-                          hintText: "Password", border: OutlineInputBorder())),
-                  const SizedBox(height: 10),
-                  TextField(
-                      controller: confirmPassController,
-                      decoration: const InputDecoration(
-                          hintText: "Confirm Password",
-                          border: OutlineInputBorder())),
-                  const SizedBox(height: 10),
-                  ElevatedButton(
-                    onPressed: () {
-                      checkValidation();
+      resizeToAvoidBottomInset: true,
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Register",
+                  style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black45),
+                ),
+                const SizedBox(),
+                TextField(
+                    controller: userNameController,
+                    decoration: const InputDecoration(
+                        hintText: "User name", border: OutlineInputBorder())),
+                const SizedBox(height: 10),
+                TextField(
+                    controller: mobileNumberController,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                        hintText: "Mobile Numebr",
+                        border: OutlineInputBorder())),
+                const SizedBox(height: 10),
+                TextField(
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                        hintText: "Email Id", border: OutlineInputBorder())),
+                const SizedBox(height: 10),
+                TextField(
+                    controller: passwordController,
+                    onChanged: (value) {
+                      checkAutoValidation(value);
                     },
-                    child: const Text("Register"),
-                  ),
-                  SizedBox(height: 10),
-                  // a minimum of 1 lower case letter [a-z] and
-                  // a minimum of 1 upper case letter [A-Z] and
-                  // a minimum of 1 numeric character [0-9] and
-                  // a minimum of 1 special character: ~`!@#$%^&*()-_+={}[]|\;:"<>,./?
-                  Row(
-                    children: [
-                      Icon(Icons.circle),
-                      Text("minimum of 1 lower case letter [a-z]")
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Icon(Icons.circle),
-                      Text("minimum of 1 upper case letter [A-Z]")
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Icon(Icons.circle),
-                      Text("Minimum of 1 numeric character [0-9]")
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Icon(Icons.circle),
-                      Text("Minimum of 1 special character")
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Icon(Icons.circle),
-                      Text("Password Length Should be 8")
-                    ],
-                  )
-                ],
-              ),
+                    decoration: const InputDecoration(
+                        hintText: "Password", border: OutlineInputBorder())),
+                const SizedBox(height: 10),
+                TextField(
+                    controller: confirmPassController,
+                    decoration: const InputDecoration(
+                        hintText: "Confirm Password",
+                        border: OutlineInputBorder())),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    checkValidation();
+                  },
+                  child: const Text("Register"),
+                ),
+                SizedBox(height: 10),
+                // a minimum of 1 lower case letter [a-z] and
+                // a minimum of 1 upper case letter [A-Z] and
+                // a minimum of 1 numeric character [0-9] and
+                // a minimum of 1 special character: ~`!@#$%^&*()-_+={}[]|\;:"<>,./?
+                Row(
+                  children: [
+                    Icon(Icons.circle,
+                        size: 16,
+                        color:
+                            isLowerCase == true ? Colors.green : Colors.grey),
+                    Text(
+                      "minimum of 1 lower case letter [a-z]",
+                      style: TextStyle(
+                          fontWeight: isLowerCase == true
+                              ? FontWeight.bold
+                              : FontWeight.normal,
+                          color:
+                              isLowerCase == true ? Colors.black : Colors.grey),
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.circle,
+                        size: 16,
+                        color:
+                            isUpperCase == true ? Colors.green : Colors.grey),
+                    Text(
+                      "minimum of 1 upper case letter [A-Z]",
+                      style: TextStyle(
+                          fontWeight: isUpperCase == true
+                              ? FontWeight.bold
+                              : FontWeight.normal,
+                          color:
+                              isUpperCase == true ? Colors.black : Colors.grey),
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.circle,
+                        size: 16,
+                        color: oneLatter == true ? Colors.green : Colors.grey),
+                    Text(
+                      "Minimum of 1 numeric character [0-9]",
+                      style: TextStyle(
+                          fontWeight: oneLatter == true
+                              ? FontWeight.bold
+                              : FontWeight.normal,
+                          color:
+                              oneLatter == true ? Colors.black : Colors.grey),
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.circle,
+                        size: 16,
+                        color: specialCharacter == true
+                            ? Colors.green
+                            : Colors.grey),
+                    Text(
+                      "Minimum of 1 special character",
+                      style: TextStyle(
+                          fontWeight: specialCharacter == true
+                              ? FontWeight.bold
+                              : FontWeight.normal,
+                          color: specialCharacter == true
+                              ? Colors.black
+                              : Colors.grey),
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.circle,
+                        size: 16,
+                        color: character == true ? Colors.green : Colors.grey),
+                    Text(
+                      "Password Length Should be 8",
+                      style: TextStyle(
+                          fontWeight: character == true
+                              ? FontWeight.bold
+                              : FontWeight.normal,
+                          color:
+                              character == true ? Colors.black : Colors.grey),
+                    )
+                  ],
+                )
+              ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
+  }
+
+  checkAutoValidation(String value) {
+    if (value.length >= 8) {
+      character = true;
+    } else {
+      character = false;
+    }
+    if (value.contains(RegExp(r'[_\-=@,.;+!#$%^&*()]'))) {
+      specialCharacter = true;
+    } else {
+      specialCharacter = false;
+    }
+    if (value.contains(RegExp(r'[a-z]'))) {
+      isLowerCase = true;
+    } else {
+      isLowerCase = false;
+    }
+    if (value.contains(RegExp(r'[A-Z]'))) {
+      isUpperCase = true;
+    } else {
+      isUpperCase = false;
+    }
+
+    if (value.contains(RegExp(r'[0-9]'))) {
+      oneLatter = true;
+    } else {
+      oneLatter = false;
+    }
+    setState(() {});
   }
 
   checkValidation() {
@@ -116,8 +214,8 @@ class _RegisterState extends State<Register> {
     String email = emailController.text.toString();
     String pass = passwordController.text.toString();
     String cPass = confirmPassController.text.toString();
-    RegExp regex =
-        RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$');
+    // RegExp regex =
+    //     RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$');
 
     if (name.isNotEmpty &&
         number.isNotEmpty &&
@@ -126,9 +224,6 @@ class _RegisterState extends State<Register> {
         cPass.isNotEmpty) {
       if (!(number.length <= 10 && number.length >= 10)) {
         showSnackBar("Mobile Number Not Valid");
-      } else if (!(pass.contains(regex))) {
-        showSnackBar(
-            "One uppercase,lowercase,symbol,latter, && Password length should be 8");
       } else if (pass != cPass) {
         showSnackBar("Password Are Not Match");
       } else {
